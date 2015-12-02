9b1d734c92c105a5169726aa74e1adf672a35e97
Drífa Örvarsdóttir <drifa15@ru.is>
2015-12-02 01:44:02 +0000

    no message

--- famouspeople.cpp
+++ famouspeople.cpp
@@ -110,6 +110,7 @@ void FamousPeople::getInfo()
         cin.clear();
         getline(cin, name);
         check = false;
+        //athuga hvort innslátturinn innihaldi nokkuð tölur
         for(unsigned int i = 0; i < name.size(); i++)
         {
             if(isdigit(name[i]))
