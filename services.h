251a6c40d3ef7b727f1a07aa70929564a973aaab
Drífa Örvarsdóttir <drifa15@ru.is>
2015-12-07 19:44:04 +0000

    fds

--- services.h
+++ services.h
@@ -1,14 +1,17 @@
 #ifndef SERVICES_H
 #define SERVICES_H
+
+#include "ui.h"
 #include <cstdlib>
 #include <cctype>
 #include <algorithm>
-#include <string>
 #include "data.h"
 #include "infotype.h"
 #include "comptype.h"
 #include "relationstype.h"
-#include "ui.h"
+
+#include <string>
+
 
 using namespace std;
 
@@ -16,19 +19,58 @@ class Services
 {
 public:
     Services();
-    //ADD FÖLLIN
+
+
+    void addRelation(int personId, int computerId);
+
     void addPerson(string name, char gender, int bYear, int dYear);
     void addComputer(string compName, int yearMade, string type, int wasBuilt);
-    //MAKE FÖLLIN
-    vector<InfoType> makePersonsVector();
-    vector<CompType> makeComputerVector();
-    void makeRelation();
-    //VIEW FÖLLIN
+
+    //tekur skjalið og setur inn í vektor
     vector<InfoType> viewPersonsInfo();
-    vector<CompType> viewComputerInfo();
+    string changeName(InfoType p);
+   /* void sortByNameAsc();
+    void sortByNameDesc();
+    void sortByYearAsc();
+    void sortByYearDesc();
+    void sortByGenderMale();
+    void sortByGenderFemale();
+    void sortByGenderUndecided();
+    void sortByDeathYearAsc();
+    void sortByDeathYearDesc();*/
+    void displaySortedPerson(vector<InfoType> FP);
+   // void sortByDeceased();
+    //void sortByNotDeceased();
+
+
+    void makeRelation();
     void viewRelationPerson();
     void viewRelationComputer();
-    //SORT PERSONS FÖLLIN
+
+    void displaySorted(int i, vector<InfoType> FP);
+    void backToSortMenu();
+
+
+
+    void searchVector();
+    //leitar í vektornum úr skjalinu
+    /*void searchVectorName();
+    void searchVectorGender();
+    void searchVectorBirthYear();
+    void searchVectorDeathYear();
+    void backToSearchMenu();*/
+    //void viewComputerInfo();
+
+    //void searchVectorComputersName();
+
+    vector<CompType> makeComputerVector();
+    vector<InfoType> makePersonsVector();
+
+    vector<InfoType> viewPersonsInfo();
+    vector<CompType> viewComputerInfo();
+
+    string changeName(InfoType p);
+
     vector <InfoType> sortByNameAsc();
     vector <InfoType> sortByNameDesc();
     vector <InfoType> sortByYearAsc();
@@ -40,7 +82,7 @@ public:
     vector <InfoType> sortByDeathYearDesc();
     vector <InfoType> sortByDeceased();
     vector <InfoType> sortByNotDeceased();
-    //SORT COMPUTER FÖLLIN
+
     vector <CompType> sortByComputerNameAsc();
     vector <CompType> sortByComputerNameDesc();
     vector <CompType> sortByYearMadeAsc();
@@ -53,17 +95,15 @@ public:
     vector <CompType> sortByYearNotBuiltDesc();
     vector <CompType> sortByYearUnknownBuiltAsc();
     vector <CompType> sortByYearUnknownBuiltDesc();
-    //SEARCH PERSONS FÖLLIN
+
     vector<InfoType> searchVectorName(string nameSearch);
     vector<InfoType> searchVectorGender(string genderSearch);
     vector<InfoType> searchVectorBirthYear(string birthYearSearch);
     vector<InfoType> searchVectorDeathYear(string deathYearSearch);
-    //SEARCH COMPUTERS FÖLLIN
+
     vector<CompType> searchVectorComputersName(string);
-    //RELATION FÖLLIN
-    void addRelation(int personId, int computerId);
-    //ANNAÐ
-    string changeName(InfoType p);
+
+    void returnToView();
 
 private:
     vector <InfoType> FP;
