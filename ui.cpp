9bb2efb48891f3eb4649b0e99c408b154dcdb406
Drífa Örvarsdóttir <drifa15@ru.is>
2015-12-09 00:08:55 +0000

    no message

--- ui.cpp
+++ ui.cpp
@@ -1933,7 +1933,7 @@ bool UI::duplicateCheckPersons(string name, char gender, int bYear, int dYear)
 {
     data p;
     Services s;
-    bool check = false;
+    bool check = true;
     vector<InfoType> FP = p.loadPersData();
     string temp1, temp2;
     temp1 = s.changeName(name);
@@ -1944,10 +1944,7 @@ bool UI::duplicateCheckPersons(string name, char gender, int bYear, int dYear)
         {
             check = false;
         }
-        else
-        {
-            check = true;
-        }
+
     }
     return check;
 }
@@ -1955,7 +1952,7 @@ bool UI::duplicateCheckComputers(string computerName, int computerYearMade, stri
 {
     data c;
     Services s;
-    bool check = false;
+    bool check = true;
     vector<CompType> Comp = c.loadCompData();
     string temp1, temp2, temp3, temp4;
 
@@ -1969,10 +1966,6 @@ bool UI::duplicateCheckComputers(string computerName, int computerYearMade, stri
         if((temp1 == temp3) && (computerYearMade == Comp.at(i).yearMade) && (temp2 == temp4) && (wasBuilt == Comp.at(i).wasBuilt))
         {
             check = false;
-        }
-        else
-        {
-            check = true;
         }
     }
     return check;
